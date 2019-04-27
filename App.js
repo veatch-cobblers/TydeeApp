606ef3e5afd896d0b946ce24204701ae83ae05b6
Lalu John <lalujohn@lalus-mbp.event.rightround.com>
2019-04-27 13:06:10 -0500

    Basic structure

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen/>
+      // <View style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      // </View>
     );
   }
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+  import { WebBrowser } from 'expo';
+
+  export default class HomeScreen extends React.Component {
+ 
+    render(){
+        return(
+            <Text>
+                THIS IS GOING TO BE THE HOME SCREEN
+            </Text>
+        )
+    }
+
+}
